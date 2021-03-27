39d95a64a96fe9046be6fbc270879eb6a11896a9
nidabijapure23 <75780814+nidabijapure23@users.noreply.github.com>
2021-03-27 21:38:16 +0530

    Update nida.py

--- nida.py
+++ nida.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 model = pickle.load(open('rf_regression_model.pkl', 'rb'))
 @app.route('/',methods=['GET'])
 def Home():
-    return render_template('index.html')
+    return render_template('index1.html')
 
 
 standard_to = StandardScaler()
